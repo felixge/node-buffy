f1801067b82f281c12b62409cf6f95cb2a6adb80
Felix Geisendörfer <felix@debuggable.com>
2012-09-04 15:23:53 +0200

    String support

--- Readme.md
+++ Readme.md
@@ -9,13 +9,13 @@ Let's say you want to parse a simple C struct, buffy can help:
 ```js
 var buffy = require('buffy');
 
-var buffer = new Buffer([23, 0, 0, 0, 15, 116, 101, 115, 116, 0]);
+var buffer = new Buffer([23, 0, 0, 0, 15, 116, 101, 115, 116]);
 var parser = buffy.createParser(buffer);
 
 var struct = {
   version : parser.uint8(),
   id      : parser.uint32(),
-  name    : parser.string(),
+  name    : parser.ascii(4),
 };
 
 // {version: 23, id: 15, name: 'test'}
@@ -31,3 +31,14 @@ Returns the next (un)signed 8 bit integer.
 
 Returns the next (un)signed 16 bit integer. Defaults to big endian unless
 invoked with LE/BE suffix.
+
+### parser.ascii([bytes]) / parser.utf8([bytes])
+
+Returns the next `bytes` as a string of the chosen encoding. If `bytes` is
+omitted, a null terminated string is assumed.
+
+
+## Error Handling
+
+The parser will throw an exception whenever an operation exceeds the boundary
+of the internal buffer.
--- test/unit/test-BufferParser.js
+++ test/unit/test-BufferParser.js
@@ -56,4 +56,55 @@ test('BufferParser', {
     var parser = new BufferParser();
     assert.strictEqual(parser.uint16, parser.uint16BE);
   },
+
+  'ascii: fixed length ascii': testParseFixedLengthAsciiWith('ascii'),
+  'ascii: fixed length snowman': testParseFixedLengthSnowmanWith('ascii'),
+  'ascii: null terminated ascii': testParseNullTerminatedAsciiWith('ascii'),
+
+  'utf8: fixed length ascii': testParseFixedLengthAsciiWith('utf8'),
+  'utf8: fixed length snowman': testParseFixedLengthSnowmanWith('utf8'),
+  'utf8: null terminated ascii': testParseNullTerminatedAsciiWith('utf8'),
 });
+
+function testParseFixedLengthAsciiWith(encoding) {
+  return function() {
+    var buffer = new Buffer([
+      'a'.charCodeAt(),
+      'b'.charCodeAt(),
+      'c'.charCodeAt(),
+      'd'.charCodeAt(),
+      'e'.charCodeAt(),
+    ]);
+    var parser = new BufferParser(buffer);
+
+    assert.equal(parser[encoding](3), 'abc');
+    assert.equal(parser[encoding](2), 'de');
+  };
+}
+
+function testParseFixedLengthSnowmanWith(encoding) {
+  return function() {
+    var buffer = new Buffer('\u2603', 'utf8');
+    var parser = new BufferParser(buffer);
+
+    assert.equal(parser[encoding](3), '☃');
+  };
+}
+
+function testParseNullTerminatedAsciiWith(encoding) {
+  return function() {
+    var buffer = new Buffer([
+      'a'.charCodeAt(),
+      'b'.charCodeAt(),
+      'c'.charCodeAt(),
+      0,
+      'd'.charCodeAt(),
+      'e'.charCodeAt(),
+      0,
+    ]);
+    var parser = new BufferParser(buffer);
+
+    assert.equal(parser[encoding](), 'abc');
+    assert.equal(parser[encoding](), 'de');
+  };
+}
