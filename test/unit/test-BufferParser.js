baef6795215d50a043566f810f6bc399d143677a
Felix Geisendörfer <felix@debuggable.com>
2012-09-04 14:33:32 +0200

    Aliases

--- test/unit/test-BufferParser.js
+++ test/unit/test-BufferParser.js
@@ -46,4 +46,14 @@ test('BufferParser', {
     assert.equal(parser.int16BE(), 1 * 256 + 127);
     assert.equal(parser.int16BE(), -128 * 256 + 255);
   },
+
+  'int16': function() {
+    var parser = new BufferParser();
+    assert.strictEqual(parser.int16, parser.int16BE);
+  },
+
+  'uint16': function() {
+    var parser = new BufferParser();
+    assert.strictEqual(parser.uint16, parser.uint16BE);
+  },
 });
